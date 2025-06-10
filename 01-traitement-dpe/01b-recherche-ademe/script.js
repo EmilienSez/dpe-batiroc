c4015cdefe7da9891cb24332c1e19f3817e4eeee
EmilienSez <127507863+EmilienSez@users.noreply.github.com>
2025-06-10 13:30:36 +0000

    Ajout page recupération via numéro Ademe

--- 01-traitement-dpe/01b-recherche-ademe/script.js
+++ 01-traitement-dpe/01b-recherche-ademe/script.js
@@ -2,6 +2,7 @@ const urlDPETertiairev2 = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe-v
 const urlDPETertiairev1 = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe-tertiaire/lines?select=*&q=";
 const urlDPENeufv2 = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe02neuf/lines?select=*&q=";
 const urlDPEExistantv2 = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe-v2-logements-existants/lines?select=*&q=";
+const urlAuditEnergetique = "https://data.ademe.fr/data-fair/api/v1/datasets/audit-opendata/lines?q=";
 
 // Menue de paramètrage : 
 let classBoutonActiver = "w-60 h-13 text-lg text-center mb-2 mt-2 px-2 py-2 border-4 border-yellow-400 text-gray-700 bg-yellow-400 rounded-xl hover:bg-yellow-400 transition";
@@ -11,11 +12,6 @@ let classLignePSimple = "block text-black-500 text-lg break-words line-clamp-1"
 let button = document.getElementById('boutonMyForm');
 let ConteneurDesCartes = document.getElementById('conteneur-des-cartes')
 let input = document.getElementById('myInput');
-let boutonDPETertiaire = document.getElementById('boutonDPETertiaire');
-let boutonLogementNeuf = document.getElementById('boutonLogementNeuf');
-let boutonLogementExistant = document.getElementById('boutonLogementExistant');
-let boutonAvant2021 = document.getElementById('boutonAvant2021');
-let boutonApres2021 = document.getElementById('boutonApres2021');
 
 let typeAPI = 1;
 let periodeAPI = 2;
@@ -23,24 +19,25 @@ let periodeAPI = 2;
 let nomColAPIv2Tertiaire = ["Score_BAN","Adresse_brute","Nom__commune_(Brut)","Code_postal_(brut)","N°DPE","Etiquette_DPE","Conso_kWhep/m²/an","Etiquette_GES",
     "Emission_GES_kgCO2/m²/an","Date_établissement_DPE","Méthode_du_DPE", "Année_construction", "Période_construction", "Secteur_activité", "Surface_(SHON)"
     , "Surface_utile"];
-let nomColAPIv1Tertiaire = ["Score_BAN","Adresse_brute","Nom__commune_(Brut)","Code_postal_(brut)","N°DPE","Etiquette_DPE","Conso_kWhep/m²/an","Etiquette_GES",
-    "Emission_GES_kgCO2/m²/an","Date_établissement_DPE","Méthode_du_DPE", "Année_construction", "Période_construction", "Secteur_activité", "Surface_(SHON)"
-    , "Surface_utile"];
+
+let nomColAPIv1Tertiaire = ["geo_score", "nom_rue", "commune", "code_postal", "numero_dpe", "classe_consommation_energie", "consommation_energie", 
+    "classe_estimation_ges", "estimation_ges", "date_etablissement_dpe", "nom_methode_dpe", "annee_construction", "annee_construction", "secteur_activite", "shon",
+    "surface_utile"];
+
 let nomColAPIv2Neuf = ["score_ban","adresse_brut","nom_commune_brut","code_postal_brut","numero_dpe","etiquette_dpe","conso_5_usages_par_m2_ef","etiquette_ges",
     "emission_ges_5_usages_par_m2","date_etablissement_dpe","methode_application_dpe", "Année_construction", "Période_construction","type_batiment",
     "surface_habitable_immeuble","surface_habitable_logement"
 ];
-let nomColAPIv2Existant = ["geo_score","Adresse_brute","Nom__commune_(BAN)","Code_postal_(brut)","N°DPE","Etiquette_DPE","Conso_5_usages_par_m²_é_primaire",
+
+let nomColAPIv2Existant = ["Score_BAN","Adresse_brute","Nom__commune_(BAN)","Code_postal_(brut)","N°DPE","Etiquette_DPE","Conso_5_usages_par_m²_é_primaire",
     "Etiquette_GES","Emission_GES_5_usages_par_m²","Date_établissement_DPE","Méthode_application_DPE", "Année_construction", "Période_construction","Type_bâtiment"
     ,"Surface_habitable_logement","Surface_habitable_immeuble"
 ];
 
-// Par défault : 
-boutonDPETertiaire.className = classBoutonActiver;
-boutonLogementNeuf.className = classBoutonDesactiver;
-boutonLogementExistant.className = classBoutonDesactiver;
-boutonAvant2021.className = classBoutonDesactiver;
-boutonApres2021.className = classBoutonActiver;
+let nomColAPIv2Audit = ["score_ban","adresse_brut","nom_commune_brut","code_postal_brut","numero_dpe","classe_bilan_dpe","conso_5_usages_m2",
+    "etiquette_ges","emission_ges_5_usages_m2","date_etablissement_audit","methode_application_dpe", "annee_construction", "periode_constuction","Type_bâtiment"
+    ,"surface_habitable_logement","surface_ventilee"
+];
 
 function getChoixAPI(typeAPIf, periodeAPIf, num_ademe) {
     if (typeAPIf === 1 && periodeAPIf === 1) {
@@ -51,6 +48,8 @@ function getChoixAPI(typeAPIf, periodeAPIf, num_ademe) {
         urlGetf = `${urlDPENeufv2}${num_ademe}&q_mode=simple&q_fields=numero_dpe`;
     } else if (typeAPIf === 3 && periodeAPIf === 2) {
         urlGetf = `${urlDPEExistantv2}${num_ademe}&q_mode=simple&q_fields=N°DPE`;
+    } else if (typeAPIf === 4 && periodeAPIf === 2) {
+        urlGetf = `${urlAuditEnergetique}${num_ademe}&q_mode=simple&q_fields=n_audit`;
     }
     return urlGetf
 };
@@ -58,22 +57,41 @@ function getChoixAPI(typeAPIf, periodeAPIf, num_ademe) {
 function getChoixColonneAPI(typeAPIf, periodeAPIf) {
     let listeColonneAPI = [];
     if (typeAPIf === 1 && periodeAPIf === 1) {
-        listeColonneAPI = nomColAPIv2Tertiaire;
+        listeColonneAPI = nomColAPIv1Tertiaire;
     } else if (typeAPIf === 1 && periodeAPIf === 2) {
         listeColonneAPI = nomColAPIv2Tertiaire;
     } else if (typeAPIf === 2 && periodeAPIf === 2) {
         listeColonneAPI = nomColAPIv2Neuf;
     } else if (typeAPIf === 3 && periodeAPIf === 2) {
         listeColonneAPI = nomColAPIv2Existant;
+    } else if (typeAPIf === 4 && periodeAPIf === 2) {
+        listeColonneAPI = nomColAPIv2Audit;
     }
     return listeColonneAPI
 }
 
+function getNomAPI(typeAPIf, periodeAPIf, num) {
+    let nomAPIDPE = "";
+    if (typeAPIf === 1 && periodeAPIf === 1) {
+        nomAPIDPE = `DPE Tertiaire Avant Juillet 2021 (${num} DPE)`;
+    } else if (typeAPIf === 1 && periodeAPIf === 2) {
+        nomAPIDPE = `DPE Tertiaire Après Juillet 2021 (${num} DPE)`;
+    } else if (typeAPIf === 2 && periodeAPIf === 2) {
+        nomAPIDPE = `DPE Logement Neuf Après Juillet 2021 (${num} DPE)`;
+    } else if (typeAPIf === 3 && periodeAPIf === 2) {
+        nomAPIDPE = `DPE Logement Existant Après Juillet 2021 (${num} DPE)`;
+    } else if (typeAPIf === 4 && periodeAPIf === 2) {
+        nomAPIDPE = `Audit Energétique Après Juillet 2021 (${num} DPE)`;
+    }
+    return nomAPIDPE
+}
 
 // Fonction pour aller récupérer les DPE associé au geo_id
-async function getInfoDPE(num_ademe) {
-    let urlGet = getChoixAPI(typeAPI, periodeAPI, num_ademe)
-    let listeColonneAPI = getChoixColonneAPI(typeAPI, periodeAPI)
+async function getInfoDPE(num_ademe, type, prd, liste) {
+
+    let urlGet = getChoixAPI(type, prd, num_ademe);
+    // let listeColonneAPI = getChoixColonneAPI(typeAPI, periodeAPI);
+    let div = creationDivVide();
     const requete = await fetch(urlGet, {
         method: 'GET'
     });
@@ -82,14 +100,22 @@ async function getInfoDPE(num_ademe) {
         alert('Un problème est survenu, veuillez réessayer plus tard');
     } else {
         let donnees = await requete.json();
+        let nom = getNomAPI(type, prd, donnees.total);
+        let elementNomAPI = creationNomAPI(nom);
         console.log(donnees)
-        ConteneurDesCartes.innerHTML = ""; 
+        // ConteneurDesCartes.innerHTML = ""; 
         if (donnees.total == 0) {
             let msg = creationMessageVide();
-            ConteneurDesCartes.appendChild(msg);
+            div.appendChild(elementNomAPI);
+            div.appendChild(msg);
+            ConteneurDesCartes.appendChild(div);
         } else {
-            carteDPE = creationCarteDPE(donnees, listeColonneAPI);
-            ConteneurDesCartes.appendChild(carteDPE);
+            div.appendChild(elementNomAPI);
+            for (let index = 0; index < donnees.total; index++) {
+                carteDPE = creationCarteDPE(donnees, liste, index);
+                div.appendChild(carteDPE);
+            }
+            ConteneurDesCartes.appendChild(div);
         }
     }
 };
@@ -97,13 +123,24 @@ async function getInfoDPE(num_ademe) {
 
 function creationMessageVide() {
     const msg = document.createElement('p');
-    msg.className = "flex justify-center text-2xl text-red-500";
+    msg.className = "flex justify-center text-xl text-red-500";
     msg.textContent = "Ce numéro ADEME n'est pas récupèrable dans cette base de données";
     return msg
 }
 
+function creationNomAPI(texte) {
+    const msg = document.createElement('p');
+    msg.className = "flex justify-center text-2xl underline bold";
+    msg.textContent = texte;
+    return msg
+}
+
+function creationDivVide() {
+    const div = document.createElement('div');
+    return div
+}
 // Fonction pour ajouter une carte DPE
-function creationCarteDPE(donnees, listeColonne) {
+function creationCarteDPE(donnees, listeColonne, index) {
 
     // Création des informations des DPE :
     const liCardDPE = document.createElement('li');
@@ -127,7 +164,7 @@ function creationCarteDPE(donnees, listeColonne) {
     // Changement des classes pour Front : 
     cardDivDPEEnglobe.className = "flex justify-center"
     cardDivDPECarte.className = "bg-[#a5b68d] ml-[5%] rounded-2xl overflow-hidden shadow-md flex flex-col p-2 w-[55%] h-auto border-4 border-[#fffade]"
-    cardDivDPECarte.id = `carte_numero_dpe`
+    cardDivDPECarte.id = `carte_numero_dpe_${index}`
     cardH1Titre.className = "text-center font-bold text-2xl mb-2"
     cardDivDPEObjet.className = "flex items-center"
     cardImgDPE.className = "w-16 h-16 mr-2 object-cover hidden sm:block"
@@ -149,23 +186,23 @@ function creationCarteDPE(donnees, listeColonne) {
     cardImgDPE.src = "../../static/DPE.png"
 
     // Changement du texte :
-    cardH1Titre.textContent = `DPE n°1 : ${donnees.results[0][listeColonne[0]]} - ${donnees.results[0][listeColonne[1]]}, ${donnees.results[0][listeColonne[2]]} ${donnees.results[0][listeColonne[3]]}`;
-    cardPTitreDoneesSaisie.textContent = `N° ADEME : ${donnees.results[0][listeColonne[4]]}`;
-    cardPEtiquetteDPE.textContent = `Etiquette DPE : ${donnees.results[0][listeColonne[5]]} - ${donnees.results[0][listeColonne[6]]} kWhep/m²/an`;
-    cardPEtiquetteGES.textContent = `Etiquette GES : ${donnees.results[0][listeColonne[7]]} - ${donnees.results[0][listeColonne[8]]} kgCO2/m²/an`;
-    cardPDateEtablissement.textContent = `Date DPE : ${donnees.results[0][listeColonne[9]]}`;
-    cardPMethodeDPE.textContent = `Méthode du DPE : ${donnees.results[0][listeColonne[10]]}`;
+    cardH1Titre.textContent = `DPE n°1 : ${Math.round(donnees.results[index][listeColonne[0]]*100)} % - ${donnees.results[index][listeColonne[1]]}, ${donnees.results[index][listeColonne[2]]} ${donnees.results[index][listeColonne[3]]}`;
+    cardPTitreDoneesSaisie.textContent = `N° ADEME : ${donnees.results[index][listeColonne[4]]}`;
+    cardPEtiquetteDPE.textContent = `Etiquette DPE : ${donnees.results[index][listeColonne[5]]} - ${donnees.results[index][listeColonne[6]]} kWhep/m²/an`;
+    cardPEtiquetteGES.textContent = `Etiquette GES : ${donnees.results[index][listeColonne[7]]} - ${donnees.results[index][listeColonne[8]]} kgCO2/m²/an`;
+    cardPDateEtablissement.textContent = `Date DPE : ${donnees.results[index][listeColonne[9]]}`;
+    cardPMethodeDPE.textContent = `Méthode du DPE : ${donnees.results[index][listeColonne[10]]}`;
     let anneeConstruction;
-    if (!donnees.results[0][listeColonne[11]]) {
-        anneeConstruction = donnees.results[0][listeColonne[12]];
+    if (!donnees.results[index][listeColonne[11]]) {
+        anneeConstruction = donnees.results[index][listeColonne[12]];
     } else {
-        anneeConstruction = donnees.results[0][listeColonne[11]];
+        anneeConstruction = donnees.results[index][listeColonne[11]];
     };
 
-    cardPSecteurActivite.textContent = `Secteur d'activité : ${donnees.results[0][listeColonne[13]]}`;
+    cardPSecteurActivite.textContent = `Secteur d'activité : ${donnees.results[index][listeColonne[13]]}`;
     cardPAnneeConstruction.textContent = `Année de construction : ${anneeConstruction}`;
-    cardPSurfaceShon.textContent = `Surface SHON : ${donnees.results[0][listeColonne[14]]}`;
-    cardPSurfaceUtile.textContent = `Surface Utile : ${donnees.results[0][listeColonne[15]]}`;
+    cardPSurfaceShon.textContent = `Surface SHON : ${donnees.results[index][listeColonne[14]]}`;
+    cardPSurfaceUtile.textContent = `Surface Utile : ${donnees.results[index][listeColonne[15]]}`;
 
     // Ajout à la carte (div de vérif) : 
     cardDivDPEDoneesVerif.appendChild(cardPMethodeDPE);
@@ -206,68 +243,20 @@ function creationCarteDPE(donnees, listeColonne) {
 
 // Exemple de Recherche ADEME : https://data.ademe.fr/data-fair/api/v1/datasets/dpe-v2-tertiaire-2/lines?select=*&q=2349T0317045P&q_modr=simple&q_fields=N%C2%B0DPE
 // Exemple de Recherche ADEME v1 : https://data.ademe.fr/data-fair/api/v1/datasets/dpe-tertiaire/lines?select=*&q=1330V8000001A&q_mode=simple&q_field=numero_dpe
+// Exemple Audit : https://data.ademe.fr/data-fair/api/v1/datasets/audit-opendata/lines?q=A23770111876U&q_mode=simple&q_fields=n_audit
 
 button.addEventListener('click', (e) => {
     globalData = []
+    ConteneurDesCartes.innerHTML = ""; 
     let valueSend = input.value
-    console.log(valueSend);
+    // console.log(valueSend);
     if (isNaN(valueSend) == true && valueSend.length < 150) {
-        getInfoDPE(valueSend)
+        getInfoDPE(valueSend,1,1,nomColAPIv1Tertiaire);
+        getInfoDPE(valueSend,1,2,nomColAPIv2Tertiaire);
+        getInfoDPE(valueSend,2,2,nomColAPIv2Neuf);
+        getInfoDPE(valueSend,3,2,nomColAPIv2Existant);
+        getInfoDPE(valueSend,4,2,nomColAPIv2Audit);
     } else {
         alert("Veuillez saisir une adresse correcte");
     }
-})
-    ;
-
-boutonDPETertiaire.addEventListener('click', (e) => {
-    if (boutonDPETertiaire.className != classBoutonActiver) {
-        boutonLogementNeuf.className = classBoutonDesactiver;
-        boutonLogementExistant.className = classBoutonDesactiver;
-        boutonDPETertiaire.className = classBoutonActiver;
-        typeAPI = 1;
-        boutonAvant2021.style.display = 'block';
-    }
-})
-
-boutonLogementNeuf.addEventListener('click', (e) => {
-    if (boutonLogementNeuf.className != classBoutonActiver) {
-        boutonDPETertiaire.className = classBoutonDesactiver;
-        boutonLogementExistant.className = classBoutonDesactiver;
-        boutonLogementNeuf.className = classBoutonActiver;
-        boutonAvant2021.style.display = 'none';
-        boutonAvant2021.className = classBoutonDesactiver;
-        boutonApres2021.className = classBoutonActiver;
-        periodeAPI = 2;
-        typeAPI = 2;
-    }
-})
-
-boutonLogementExistant.addEventListener('click', (e) => {
-    if (boutonLogementExistant.className != classBoutonActiver) {
-        boutonDPETertiaire.className = classBoutonDesactiver;
-        boutonLogementNeuf.className = classBoutonDesactiver;
-        boutonLogementExistant.className = classBoutonActiver;
-        boutonLogementExistant.className = classBoutonActiver;
-        boutonAvant2021.style.display = 'none';
-        boutonAvant2021.className = classBoutonDesactiver;
-        boutonApres2021.className = classBoutonActiver;
-        periodeAPI = 2;
-        typeAPI = 3;
-    }
-})
-
-boutonAvant2021.addEventListener('click', (e) => {
-    if (boutonAvant2021.className != classBoutonActiver) {
-        boutonApres2021.className = classBoutonDesactiver;
-        boutonAvant2021.className = classBoutonActiver;
-        periodeAPI = 1;
-    }
-})
-
-boutonApres2021.addEventListener('click', (e) => {
-    if (boutonApres2021.className != classBoutonActiver) {
-        boutonAvant2021.className = classBoutonDesactiver;
-        boutonApres2021.className = classBoutonActiver;
-        periodeAPI = 2;
-    }
-})
+});
